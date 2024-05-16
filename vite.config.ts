7fb935f0cc742dbb57207a8f0141427a75883509
Lee sang Yeop <mma7710@naver.com>
2024-05-16 15:58:05 +0900

    deploy: 배포

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://pp449.github.io/react-shopping-cart/dist/",
   plugins: [react()],
-})
+});
